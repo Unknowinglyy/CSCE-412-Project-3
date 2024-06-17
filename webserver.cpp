ce46e6ef78540ad329d61825d894505d799da4d1
Unknowinglyy <unknowingly@tamu.edu>
2024-06-16 22:00:28 -0500

    webserver weirdness

--- webserver.cpp
+++ webserver.cpp
@@ -3,17 +3,22 @@
 #include <chrono>
 #include <thread>
 void webserver::processRequest(){
-    std::cout << "Processing request..." << std::endl;
-    std::cout << "Input IP: " << personalQueue.front().getIPIn() << std::endl;
-    std::cout << "Output IP: " << personalQueue.front().getIPOut() << std::endl;
-    //get the process time
-    int time = personalQueue.front().getProcessTime();
-    //simulate the process time by sleeping for time milliseconds
-    std::this_thread::sleep_for(std::chrono::milliseconds(time));
-    //pop the request from the queue
-    personalQueue.pop();
-    std::cout << "Request processed." << std::endl;
-    std::cout << "Request took " << time << " milliseconds." << std::endl << std::endl;
+    if(!personalQueue.empty()){
+        std::cout << "Processing request..." << std::endl;
+        std::cout << "Input IP: " << personalQueue.front().getIPIn() << std::endl;
+        std::cout << "Output IP: " << personalQueue.front().getIPOut() << std::endl;
+        //get the process time
+        int time = personalQueue.front().getProcessTime();
+        //simulate the process time by sleeping for time milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+        //pop the request from the queue
+        personalQueue.pop();
+        std::cout << "Request processed." << std::endl;
+        std::cout << "Request took " << time << " milliseconds." << std::endl << std::endl;
+    }
+    else{
+        std::cout << "No requests to process for this server." << std::endl;
+    }
 }
 void webserver::loadRequest(request r){
     //load the request into the queue
